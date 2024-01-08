8c3955f5c84f2be9e20a154fec131eaeb6042d76
aiithak <andrewskeller@gmaill.com>
2024-01-08 15:30:30 -0300

    Setup android compose

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
     repositories {
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         google()
         gradlePluginPortal()
         mavenCentral()
@@ -13,5 +14,8 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
+
+include(":composeApp")
