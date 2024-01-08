6319a9965cc3dcde106e46af212fa0ef10a92e4d
aiithak <andrewskeller@gmaill.com>
2024-01-08 15:36:15 -0300

    Setup ios compose

---
+++ composeApp/build.gradle.kts
@@ -0,0 +1,35 @@
+import org.jetbrains.compose.ExperimentalComposeLibrary
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    
+    alias(libs.plugins.jetbrainsCompose)
+}
+
+kotlin {
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "ComposeApp"
+            isStatic = true
+        }
+    }
+    
+    sourceSets {
+        
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material)
+            implementation(compose.ui)
+            @OptIn(ExperimentalComposeLibrary::class)
+            implementation(compose.components.resources)
+        }
+    }
+}
+
+
---
+++ composeApp/src/iosMain/kotlin/Platform.kt
@@ -0,0 +1,7 @@
+import platform.UIKit.UIDevice
+
+class IOSPlatform {
+    val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+}
+
+fun getPlatform() = IOSPlatform()
